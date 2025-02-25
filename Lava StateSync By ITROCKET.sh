fa4296aa1952daf8b9dd55502af8fe3b00470f87
TrustTech <161213942+Dr0ff@users.noreply.github.com>
2025-02-25 08:22:22 +0000

    Update Lava StateSync By ITROCKET.sh

--- Lava StateSync By ITROCKET.sh
+++ Lava StateSync By ITROCKET.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+echo "Настроим StateSync? Нажми любую клавишу"
+read -n 1 -s
+echo "Останавливаю сервис! Нажми любую клавишу"
+read -n 1 -s
 sudo systemctl stop lava.service
 
+echo "Сохраняю Vaidator State и делаю Сброс и очистку ноды! Нажми любую клавишу"
+read -n 1 -s
 cp $HOME/.lava/data/priv_validator_state.json $HOME/.lava/priv_validator_state.json.backup
 lavad tendermint unsafe-reset-all --home $HOME/.lava --keep-addr-book
 
+echo "Добавляю Peers! Нажми любую клавишу"
+read -n 1 -s
 peers="0d67bedc7f929200d52c8724dfc50f848661f9ba@lava-mainnet-peer.itrocket.net:28656,8d28c38d956384510558664f5897a383b7529699@136.243.95.31:29156,859a5b9a8c3d9ef51abbf8c0ef29bdf3eaf142af@35.224.202.186:26656,408ddeb68bd2cc5e6ff1b3ed17ac1e79b70cb356@65.108.111.236:55676,ab7a906235396f2a5ddfce73375430c7ac5e0097@23.227.223.1:26656,f0731f48574c0e8d3dd6716a9ae45f878dd7dc9e@[2a01:4f9:3080:419e::9]:26656,e726816f42831689eab9378d5d577f1d06d25716@169.155.171.209:26656,f6a239ae943247ec5f879866b44648b64a85a654@168.119.89.91:41004,e94feefdb4f9b2fac84cf3fc0b1795ce03bb82c2@84.17.42.200:56456,35699d219003077eb6112e971ae2a20c0cb80060@162.55.29.50:26656,e29416e0884674d946f095f9a4ddeee7a332d0ac@65.109.118.161:26656"  
 SNAP_RPC="https://lava-mainnet-rpc.itrocket.net:443"
 
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.lava/config/config.toml 
 
+echo "Вычисляю нужный нам блок! Нажми любую клавишу"
+read -n 1 -s
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height);
 BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000));
 TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash) 
@@ -21,7 +31,11 @@ s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ;
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ;
 s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.lava/config/config.toml
 
+echo "Возвращаю Validator State! Нажми любую клавишу"
+read -n 1 -s
 mv $HOME/.lava/priv_validator_state.json.backup $HOME/.lava/data/priv_validator_state.json
 
+echo "Готово! Запускаю ноду и журнал! Нажми любую клавишу"
+read -n 1 -s
 sudo systemctl start lava.service
 sudo journalctl -u lava -f --output cat
