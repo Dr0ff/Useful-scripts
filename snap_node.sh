e23a8315f92822d1badfb6596a9cd4a32b9d5592
Dr0ff <161213942+Dr0ff@users.noreply.github.com>
2025-02-28 00:31:48 +0000

    Selection of network for snapshot added

--- snap_node.sh
+++ snap_node.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
 
-block=$(lavad status | jq -r '.SyncInfo.latest_block_height')
+while true; do
+    # Запрашиваем данные
+    read -p "Enter your node Binary name (eg: junod, lavad, xplad): " binary
+    read -p "Enter your node Network or directory name (eg: juno, lava, sommelier): " network
+    
+    # Выводим данные для проверки
+    echo " "
+    echo -e "Your node's Binary file name: \e[33m$binary\e[0m"
+    echo -e "Your node's Network/Directory name: \e[33m$network \e[0m"
+    echo " "
+    
+    # Спрашиваем пользователя, всё ли верно
+    read -p "All good? Yes/No: " answer
+    case $answer in
+        [Yy]*)
+            break  # Прерывает цикл и продолжает выполнение
+            ;;
+        [Nn]*)
+            echo "Let's try again."
+            # Цикл продолжится, данные будут запрашиваться заново
+            ;;
+        *)
+            echo "Please answer 'Yes' or 'No'."
+            # Цикл продолжится, если введено что-то другое
+            ;;
+    esac
+done
+sudo -v
+block=$($binary status | jq -r '.SyncInfo.latest_block_height')
+snap_file=~/"${network}_snap_$(date +%Y-%m-%d)_$block.tar.lz4"
+echo -e "\n\e[33mYour snapshot will be saved to: $snap_file\e[0m"
 
 # Останавливаем ноду
-sudo systemctl stop lava.service
+sudo systemctl stop $network.service
 
 # Создаём снапшот, сохраняя только `data/`
-tar -cf - -C ~/.lava data | lz4 -9 > ~/lava_snapshot_$(date +%Y-%m-%d)_$block.tar.lz4
+#tar -cf - -C ~/.$network data | lz4 -9 > ~/$network_snap_$(date +%Y-%m-%d)_$block.tar.lz4
+tar -cf - -C ~/.$network data | lz4 -9 > $snap_file
 
 # Запускаем ноду
-sudo systemctl start lava.service
+sudo systemctl start $network.service
+
