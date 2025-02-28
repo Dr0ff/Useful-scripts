87f8f689823d246e14bb97a51932684482d14da9
TrustTech <161213942+Dr0ff@users.noreply.github.com>
2025-02-28 01:05:08 +0000

    Update snap_node.sh

--- snap_node.sh
+++ snap_node.sh
@@ -30,8 +30,8 @@ done
 sudo -v
 block=$($binary status | jq -r '.SyncInfo.latest_block_height')
 snap_file=~/"${network}_snap_$(date +%Y-%m-%d)_$block.tar.lz4"
-echo -e "⚠️  Your snapshot will be saved to: \n\e[33m$snap_file\e[0m"
-echo -e "⚠️  To unpack your snapshot use:\n\e[33mlz4 -c -d $snap_file | tar -x -C ~/.$network\e[0m"
+echo -e "Your snapshot will be saved to: \n\e[33m$snap_file\e[0m"
+echo -e "To unpack your snapshot use:\n\e[33mlz4 -c -d $snap_file | tar -x -C ~/.$network\e[0m"
 exit 1
 # Останавливаем ноду
 sudo systemctl stop $network.service
@@ -41,5 +41,4 @@ tar -cf - -C ~/.$network data | lz4 -9 > $snap_file
 
 # Запускаем ноду
 sudo systemctl start $network.service
-
-echo -e "\nTo unpacking your snapshot use:\n\e[33mlz4 -c -d $snap_file | tar -x -C ~/.$network\e[0m"
+echo -e "We restarting your $network node again. \n To check it just run folowing: \e\[33msudo journalctl -u $network -f --output cat\e[0m "
