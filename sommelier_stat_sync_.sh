e6261a9fed6e746474f0217234536f9462057387
TrustTech <161213942+Dr0ff@users.noreply.github.com>
2025-03-16 19:26:44 +0000

    Update sommelier_stat_sync_.sh

--- sommelier_stat_sync_.sh
+++ sommelier_stat_sync_.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 ## To launch this script run this command in your node's terminal:
 ## curl https://raw.githubusercontent.com/Dr0ff/Useful-scripts/refs/heads/main/sommelier_stat_sync_.sh | bash
+SNAP_RPC="https://rpc.lavenderfive.com:443/sommelier"
+BACK_TO_BLOCKS=2000
+echo -e "\e[33mRPC NODE:\e[32m $SNAP_RPC\e[0m"
+echo -e "\e[33mBack to blocks:\e[32m $BACK_TO_BLOCKS\e[0m"
 
 sudo systemctl stop sommelier.service
 cp $HOME/.sommelier/data/priv_validator_state.json $HOME/.sommelier/priv_validator_state.json.backup
 sommelier tendermint unsafe-reset-all --home $HOME/.sommelier --keep-addr-book
 #peers=""  
-SNAP_RPC="https://rpc.lavenderfive.com:443/sommelier"
 #sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.sommelier/config/config.toml 
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height);
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000));
+BLOCK_HEIGHT=$((LATEST_HEIGHT - $BACK_TO_BLOCKS));
 TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash) 
 echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH && sleep 2
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ;
