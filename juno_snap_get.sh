819e561e3cac67b7d92e06315c3e25be29817a72
TrustTech <161213942+Dr0ff@users.noreply.github.com>
2025-03-30 12:06:44 +0100

    Update juno_snap_get.sh

--- juno_snap_get.sh
+++ juno_snap_get.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-sudo -v
-read -p "" SNAP_LINK
-echo "$SNAP_LINK"
-#wget -O juno_latest.tar.lz4 $SNAP_LINK --inet4-only
-#sudo systemctl stop juno.service
-#cp $HOME/.juno/data/priv_validator_state.json $HOME/.juno/priv_validator_state.json.backup
-#junod tendermint unsafe-reset-all --home $HOME/.juno --keep-addr-book
-#lz4 -c -d juno_latest.tar.lz4  | tar -x -C $HOME/.juno
-#mv $HOME/.juno/priv_validator_state.json.backup $HOME/.juno/data/priv_validator_state.json
-#sudo systemctl start juno.service
-#sudo journalctl -u juno -f --output cat
-#rm -v juno_latest.tar.lz4
+#This script just downloading and runing Juno Snapshot Instalation script
+wget -O https://raw.githubusercontent.com/Dr0ff/Useful-scripts/refs/heads/main/juno_snap_install.sh --inet4-only
+bash juno_snap_install.sh
