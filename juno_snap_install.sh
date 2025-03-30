e2f0a0c37fe194917bfa19c05e51978a5b9567ea
TrustTech <161213942+Dr0ff@users.noreply.github.com>
2025-03-30 12:20:58 +0100

    Update juno_snap_install.sh

--- juno_snap_install.sh
+++ juno_snap_install.sh
@@ -2,13 +2,12 @@
 sudo -v
 read -p "" SNAP_LINK
 echo "$SNAP_LINK"
-break
-wget -O juno_latest.tar.lz4 $SNAP_LINK --inet4-only
-sudo systemctl stop juno.service
-cp $HOME/.juno/data/priv_validator_state.json $HOME/.juno/priv_validator_state.json.backup
-junod tendermint unsafe-reset-all --home $HOME/.juno --keep-addr-book
-lz4 -c -d juno_latest.tar.lz4  | tar -x -C $HOME/.juno
-mv $HOME/.juno/priv_validator_state.json.backup $HOME/.juno/data/priv_validator_state.json
-sudo systemctl start juno.service
-sudo journalctl -u juno -f --output cat
-rm -v juno_latest.tar.lz4
+#wget -O juno_latest.tar.lz4 $SNAP_LINK --inet4-only
+#sudo systemctl stop juno.service
+#cp $HOME/.juno/data/priv_validator_state.json $HOME/.juno/priv_validator_state.json.backup
+#junod tendermint unsafe-reset-all --home $HOME/.juno --keep-addr-book
+#lz4 -c -d juno_latest.tar.lz4  | tar -x -C $HOME/.juno
+#mv $HOME/.juno/priv_validator_state.json.backup $HOME/.juno/data/priv_validator_state.json
+#sudo systemctl start juno.service
+#sudo journalctl -u juno -f --output cat
+#rm -v juno_latest.tar.lz4
