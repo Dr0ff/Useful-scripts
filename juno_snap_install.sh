439bc572cd5b5cfe866f0306a59d74a54a214be1
TrustTech <161213942+Dr0ff@users.noreply.github.com>
2025-03-30 12:33:43 +0100

    Update juno_snap_install.sh

--- juno_snap_install.sh
+++ juno_snap_install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 sudo -v
-read -p "Enter" SNAP_LINK
+echo -e "Этот скрипт производит очистку ноды и переустанавливает базу данных сети из снапшота!"
+echo -e "Ссылку на снапшот можно взять на сайте https://polkachu.com/tendermint_snapshots/juno"
+read -p "Вставьте сюда ссылку на снапшот: " SNAP_LINK
 echo "$SNAP_LINK"
-#wget -O juno_latest.tar.lz4 $SNAP_LINK --inet4-only
-#sudo systemctl stop juno.service
-#cp $HOME/.juno/data/priv_validator_state.json $HOME/.juno/priv_validator_state.json.backup
-#junod tendermint unsafe-reset-all --home $HOME/.juno --keep-addr-book
-#lz4 -c -d juno_latest.tar.lz4  | tar -x -C $HOME/.juno
-#mv $HOME/.juno/priv_validator_state.json.backup $HOME/.juno/data/priv_validator_state.json
-#sudo systemctl start juno.service
-#sudo journalctl -u juno -f --output cat
-#rm -v juno_latest.tar.lz4
+wget -O juno_latest.tar.lz4 $SNAP_LINK --inet4-only
+sudo systemctl stop juno.service
+cp $HOME/.juno/data/priv_validator_state.json $HOME/.juno/priv_validator_state.json.backup
+junod tendermint unsafe-reset-all --home $HOME/.juno --keep-addr-book
+lz4 -c -d juno_latest.tar.lz4  | tar -x -C $HOME/.juno
+mv $HOME/.juno/priv_validator_state.json.backup $HOME/.juno/data/priv_validator_state.json
+sudo systemctl start juno.service
+sudo journalctl -u juno -f --output cat
+rm -v juno_latest.tar.lz4
