7ec05be3f8e943a82b913b3d8b88b9009d122ec9
TrustTech <161213942+Dr0ff@users.noreply.github.com>
2025-04-05 16:15:51 +0100

    Update and rename stargaze_snap_install to stargaze_snap_install.sh

--- stargaze_snap_install.sh
+++ stargaze_snap_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 bash <(curl -s https://raw.githubusercontent.com/Dr0ff/Useful-scripts/refs/heads/main/tt.logo.sh)
-
+NAME=stargaze
+DIR=starsd
+BIN=starsd
 sudo -v
 echo -e "\n\e[33m Этот скрипт производит очистку ноды и переустанавливает базу данных сети из снапшота!\e[0m"
 echo -e "\n\e[32m Ссылку на снапшот можно взять на сайте \e[34m https://polkachu.com/tendermint_snapshots/stargaze\e[0m"
@@ -9,13 +11,13 @@ echo -e "\e[32m Правый клик на файле для загрузки и
 read -p "Вставьте сюда ссылку на снапшот: " SNAP_LINK
 echo -e "\n\e[33m Для продолжения нажмите любую клавишу \e[0m"
 read -p ""
-wget -O stargaze_latest.tar.lz4 $SNAP_LINK --inet4-only
-sudo systemctl stop stargaze.service
-cp $HOME/.starsd/data/priv_validator_state.json $HOME/.starsd/priv_validator_state.json.backup
-starsd tendermint unsafe-reset-all --home $HOME/.starsd --keep-addr-book
-rm -r ~/.starsd/wasm
-lz4 -c -d stargaze_latest.tar.lz4  | tar -x -C $HOME/.starsd
-mv $HOME/.starsd/priv_validator_state.json.backup $HOME/.starsd/data/priv_validator_state.json
-sudo systemctl start stargaze.service
-sudo journalctl -u stargaze -f --output cat
-rm -v stargaze_latest.tar.lz4
+wget -O $NAME_latest.tar.lz4 $SNAP_LINK --inet4-only
+sudo systemctl stop $NAME.service
+cp $HOME/.$DIR/data/priv_validator_state.json $HOME/.$DIR/priv_validator_state.json.backup
+$BIN tendermint unsafe-reset-all --home $HOME/.$DIR --keep-addr-book
+rm -r ~/.$DIR/wasm
+lz4 -c -d $NAME_latest.tar.lz4  | tar -x -C $HOME/.$DIR
+mv $HOME/.$DIR/priv_validator_state.json.backup $HOME/.$DIR/data/priv_validator_state.json
+sudo systemctl start $NAME.service
+sudo journalctl -u $NAME -f --output cat
+rm -v $NAME_latest.tar.lz4
